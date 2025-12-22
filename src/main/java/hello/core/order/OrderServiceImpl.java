6a4bda88ce37e55bede198d7bd090b8de06e11db
wany0530 <ljw6398@hanmail.net>
2025-12-22 23:22:11 +0900

    할인정책 내용 커밋

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,22 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService
 {
+    // 우리는 역할과 구현을 충실하게 분리했다 -> O
+    // DiscountPolicy ( 역할 ) / FixDiscountPolicy, RateDiscountPolicy ( 구현 )
+    // 다형성도 활요하고, 인터페이스와 구현 객체를 분리했다 -> O ->
+
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //DIP 위반이다.
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //DIP 위반이다.
+    private DiscountPolicy discountPolicy; //DIP 위반 X.
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice)
